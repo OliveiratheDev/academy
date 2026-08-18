b2149e5f0494be133f28c36c380e4afe4ba554de
OliveiratheDev <gabrieloliveira362978@gmail.com>
2026-08-18 18:38:40 -0300

    feat(aluno): adiciona enum de status do aluno

--- src/main/java/com/academy/gym/enums/EStatusAluno.java
+++ src/main/java/com/academy/gym/enums/EStatusAluno.java
@@ -1,5 +1,9 @@
 package com.academy.gym.enums;
 
+import jakarta.persistence.Enumerated;
+import org.springframework.web.bind.annotation.PatchMapping;
+
+
 public enum EStatusAluno {
     ATIVO,
     INATIVO
