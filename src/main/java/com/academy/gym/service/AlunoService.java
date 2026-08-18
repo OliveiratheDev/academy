f87823c68d38ab93325331fa3a5aa944fe0ab7ba
OliveiratheDev <gabrieloliveira362978@gmail.com>
2026-08-18 18:39:00 -0300

    feat(aluno): adiciona regras de negócio do aluno

--- src/main/java/com/academy/gym/service/AlunoService.java
+++ src/main/java/com/academy/gym/service/AlunoService.java
@@ -3,48 +3,63 @@ package com.academy.gym.service;
 import com.academy.gym.database.model.AlunoEntity;
 import com.academy.gym.database.repository.IAlunoRepository;
 import com.academy.gym.dtos.AlunoRequest;
-import com.academy.gym.dtos.AlunoResponse;
+
+import com.academy.gym.dtos.AlunoStatusRequest;
 import com.academy.gym.exception.NotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.security.PublicKey;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class AlunoService {
 
-    private final AlunoResponse alunoResponse;
+
 
     private final IAlunoRepository alunoRepository;
 
-    public AlunoService(AlunoResponse alunoResponse, IAlunoRepository alunoRepository) {
-        this.alunoResponse = alunoResponse;
+
+    public AlunoService(IAlunoRepository alunoRepository) {
         this.alunoRepository = alunoRepository;
+
+
+
     }
 
+    public AlunoEntity save(AlunoRequest alunoRequest) {
+        AlunoEntity aluno = new AlunoEntity();
+        aluno.setNome(alunoRequest.nome());
+        aluno.setCpf(alunoRequest.cpf());
+        aluno.setEmail(alunoRequest.email());
+        aluno.setDataNascimento(alunoRequest.dataNascimento());
+        aluno.setTelefone(alunoRequest.telefone());
+        aluno.setStatus(alunoRequest.status());
+        return alunoRepository.save(aluno);
+    }
 
-    public List<AlunoEntity> findAll() {
+    public  List<AlunoEntity> findAll() {
         return alunoRepository.findAll();
     }
 
     public AlunoEntity findById(Long id) {
         return alunoRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException("Aluno nao encontrado"));
+                .orElseThrow(() -> new NotFoundException("Aluno não encontrado"));
     }
 
     public AlunoEntity findByCpf(String cpf) {
         return alunoRepository.findByCpf(cpf)
-                .orElseThrow(() -> new NotFoundException("Aluno nao encontrado"));
+                .orElseThrow(() -> new NotFoundException("Aluno não encontrado"));
     }
 
     public AlunoEntity findByEmail(String email) {
         return alunoRepository.findByEmail(email)
-                .orElseThrow(() -> new NotFoundException("Aluno nao encontrado"));
+                .orElseThrow(() -> new NotFoundException("Aluno não encontrado"));
     }
 
-    public AlunoEntity save(AlunoEntity alunoEntity) {
-        return alunoRepository.save(alunoEntity);
+    public AlunoEntity statusUpdate(Long id, AlunoStatusRequest alunoRequest) {
+      AlunoEntity aluno = alunoRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Aluno não encontrado"));
+      aluno.setStatus(alunoRequest.status());
+      return alunoRepository.save(aluno);
     }
 }
 
