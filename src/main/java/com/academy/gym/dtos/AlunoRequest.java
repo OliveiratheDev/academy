9ca9f84843c5924e716145b76cf4db572ae0bcab
OliveiratheDev <gabrieloliveira362978@gmail.com>
2026-08-29 22:26:07 -0300

    refactor: renomeia status para matricula e ajusta dataNascimento para String em AlunoRequest

--- src/main/java/com/academy/gym/dtos/AlunoRequest.java
+++ src/main/java/com/academy/gym/dtos/AlunoRequest.java
@@ -4,6 +4,7 @@ package com.academy.gym.dtos;
 import com.academy.gym.database.model.AlunoEntity;
 import com.academy.gym.enums.EStatusAluno;
 import jakarta.persistence.Column;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -13,8 +14,8 @@ public record AlunoRequest(
         @NotNull(message = "O nome é obrigatório") @Size(max = 40) String nome,
         @NotNull(message = "O CPF é obrigatório") @Size(max = 11) String cpf,
         @NotNull(message = "O email é obrigatório") @Size(max = 140) String email,
-        @NotNull(message = "A data de nascimento é obrigatória") @Size(max = 10) LocalDate dataNascimento,
+        @NotNull(message = "A data de nascimento é obrigatória") @Size(max = 10) String dataNascimento,
         @NotNull(message = "O telefone é obrigatório") @Size(max = 11) String telefone,
-        EStatusAluno status
+        EStatusAluno matricula
 ) {
 }
